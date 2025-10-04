616fed893949f9057f4c81c2f64a08e109091a50
gauri <gaurimn1109@gmail.com>
2025-10-04 11:54:49 +0530

    Handled multiple negative numbers test case

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -26,8 +26,12 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("//;\n1;2")).to eq(3)
     end
 
-    it "raises error for negative numbers" do
+    it "raises error for single negative number" do
       expect { StringCalculator.add("1,-2,3") }.to raise_error("Negatives not allowed: -2")
+    end
+
+    it "raises error for multiple negative numbers" do
+      expect { StringCalculator.add("1,-2,3,-4") }.to raise_error("Negatives not allowed: -2,-4")
     end
 
     it "ignores numbers greater than 1000" do
