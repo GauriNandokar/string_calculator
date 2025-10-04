3aa1a85c554784433ab042b5be3286adb7396cbb
gauri <gaurimn1109@gmail.com>
2025-10-04 10:44:35 +0530

    TDD Step 5: Support custom delimiters

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,14 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    nums = numbers.split(/,|\n/).map(&:to_i)
+
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers[2], numbers[4..]
+      nums = numbers.split(delimiter).map(&:to_i)
+    else
+      nums = numbers.split(/,|\n/).map(&:to_i)
+    end
+
     nums.sum
   end
 end
